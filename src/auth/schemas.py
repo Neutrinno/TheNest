4d174ac336b396a6fa2dcb176b89608cbc08ea06
bsr <mursik1602@yandex.ru>
2024-11-04 01:00:44 +0500

    Reargonized db

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -6,9 +6,7 @@ from typing import Optional
 
 class UserRead(schemas.BaseUser[int]):
     id: int
-    username: str
     email: str
-    admission_score: int
     registered_at: Optional[datetime] = None
     is_active: bool = True
     is_superuser: bool = False
@@ -19,10 +17,8 @@ class UserRead(schemas.BaseUser[int]):
 
 
 class UserCreate(schemas.BaseUserCreate):
-    username: str
     email: str
     password: str
-    admission_score: int
     registered_at: Optional[datetime] = None
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
