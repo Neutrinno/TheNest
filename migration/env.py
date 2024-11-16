24ae7de9da07cbb7bc90d17667de720089f1ccd6
bsr <mursik1602@yandex.ru>
2024-11-16 11:14:09 +0500

    Changed db models

--- migration/env.py
+++ migration/env.py
@@ -3,7 +3,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from src.config import DB_PASS, DB_USER, DB_HOST, DB_PORT, DB_NAME
-from src.models import metadata
+from src.models import Base
 import os
 import sys
 
@@ -28,7 +28,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
