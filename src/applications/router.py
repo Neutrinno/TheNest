9e2259a98acfd9733454b961e9af6467d006f7ff
bsr <mursik1602@yandex.ru>
2024-11-19 22:47:12 +0500

    created router to add new applications

--- src/applications/router.py
+++ src/applications/router.py
@@ -1,5 +1,42 @@
+from typing import Optional
+
 from fastapi import APIRouter
+from fastapi.params import Depends
+from pydantic import BaseModel, EmailStr, Field
+from sqlalchemy.ext.asyncio import AsyncSession
 
-router = APIRouter()
+from src.applications.database import get_async_session
+from src.models import Application, RoommatePreference
+from src.setting import settings
+
+router = APIRouter(prefix = settings.application.prefix, tags=[settings.application.tags])
 
 
+class ApplicationCreate(BaseModel):
+    student_id: int
+    full_name: str
+    email: EmailStr
+    admission_score: int = Field(gt=0, lt=100)
+    preferred_student: EmailStr
+    preferred_dormitory = Optional[int]
+    preferred_floor = Optional[int]
+
+class RoommateCreate(BaseModel):
+    preferred_student: EmailStr
+
+@router.post("/", response_model= ApplicationCreate)
+async def create_application(new_application: ApplicationCreate,
+                             new_roommate: RoommateCreate,
+                             session: AsyncSession = Depends(get_async_session)):
+
+    application = Application(**new_application.dict())
+    session.add(application)
+    await session.commit()
+    await session.refresh(application)
+
+    roommate_preferance = RoommatePreference(student_id = ApplicationCreate.student_id, **new_roommate.dict())
+
+    session.add(roommate_preferance)
+    await session.commit()
+    await session.refresh(roommate_preferance)
+    return {"message": "Your application has been successfully registered"}
