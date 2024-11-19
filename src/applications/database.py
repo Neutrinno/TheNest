1a99c6ab1e3041c3f1cb695809f0039b38885167
bsr <mursik1602@yandex.ru>
2024-11-19 22:04:03 +0500

    Created function to create session connect db

--- src/applications/database.py
+++ src/applications/database.py
@@ -11,3 +11,9 @@ class Base(DeclarativeBase):
 engine = create_async_engine(DATABASE_URL, echo = True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+async def get_db():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
