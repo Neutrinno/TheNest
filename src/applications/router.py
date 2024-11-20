593913ae13cd7064ae3fb10497ad368a3be7e23c
bsr <mursik1602@yandex.ru>
2024-11-20 23:24:03 +0500

    Rewrite functions

--- src/applications/router.py
+++ src/applications/router.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 from fastapi import APIRouter
 from fastapi.params import Depends
-from sqlalchemy import select, Result
+from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.applications.database import get_async_session
-from src.applications.schemas import ApplicationCreate, RoommateCreate
-from src.models import Application, RoommatePreference
+from src.applications.schemas import ApplicationCreate
+from src.models import Application
 
 router = APIRouter()
 
 
 @router.post("/")
 async def create_application(new_application: ApplicationCreate,
-                             new_roommate: RoommateCreate,
                              session: AsyncSession = Depends(get_async_session)):
     now = datetime.now()
     application = Application(**new_application.dict(),
@@ -21,14 +20,16 @@ async def create_application(new_application: ApplicationCreate,
     await session.commit()
     await session.refresh(application)
 
-    if any(new_roommate.dict().values()):
-        roommate_preference = RoommatePreference(**new_roommate.dict())
-        session.add(roommate_preference)
-        await session.commit()
-        await session.refresh(roommate_preference)
+    query = select(Application).where(Application.student_id == application.student_id)
+    result = await session.execute(query)
+    application_id = result.scalars().first()
 
-    query = await select(application).where(application.student_id == Application.student_id)
-    result: Result = await session.execute(query)
-    application_id = result.scalars.first()
+    return {"message": f"Your application has been successfully registered. Number of application: {application_id.id if application_id else 'not found'}"}
 
-    return {"message": f"Your application has been successfully registered. Number of application: {application_id}"}
+
+
+ #   if any(new_roommate.dict().values()):
+ #       roommate_preference = RoommatePreference(**new_roommate.dict())
+   #     session.add(roommate_preference)
+    #    await session.commit()
+    #    await session.refresh(roommate_preference)
--- src/applications/schemas.py
+++ src/applications/schemas.py
@@ -5,10 +5,15 @@ class StudentId(BaseModel):
     student_id: int
 
 class ApplicationCreate(StudentId):
-    full_name: str
+    first_name: str
+    surname: str
+    middle_name: str
     admission_score: int = Field(gt=0, lt=101)
     preferred_dormitory: Optional[Annotated[int, Field(ge=0)]] = None
     preferred_floor: Optional[Annotated[int, Field(ge=0)]] = None
+    first_preferred_student: Optional[EmailStr] = None
+    second_preferred_student: Optional[EmailStr] = None
+    third_preferred_student: Optional[EmailStr] = None
 
 class RoommateCreate(StudentId):
     first_preferred_student: Optional[EmailStr] = None
