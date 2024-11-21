d972646c8116a3ba6e0a6f77b650b9985367ffea
bsr <mursik1602@yandex.ru>
2024-11-21 23:28:46 +0500

    mk function to update status

--- src/distribution/router.py
+++ src/distribution/router.py
@@ -5,23 +5,38 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.distribution.database import get_async_session
 from src.distribution.schemas import StudentList
-from src.models import Application, StudentListing
+from src.models import Application, StudentListing, Status
 
 router = APIRouter()
+
+async def status_update(session: AsyncSession = Depends(get_async_session)):
+    query = select(StudentListing).order_by(StudentListing.student_id)
+    result: Result = await session.execute(query)
+    student_list = result.scalars().all()
+
+    for student in student_list:
+        new_status = student.status
+
+        status_record = await session.execute(select(Status.student_id == student.id).limit(1))
+        status_record.status = new_status
+
+    await session.commit()
+    return {"massage: Статусы успешно обновлены"}
 
 
 @router.get("/", response_model=list[StudentList])
 async def get_distribution(session: AsyncSession = Depends(get_async_session)):
     query = select(Application.id, Application.admission_score).order_by(desc(Application.admission_score))
-    result = await session.execute(query)
+    result: Result = await session.execute(query)
     student_list = result.all()
 
     students_to_add = []
     for index, (id_, score) in enumerate(student_list):
-        status = "одобренно" if index < 67 else "в очереди"
-        students_to_add.append(StudentListing(id=id_, admission_score=score, status=status))
+        status = "Одобренно" if index < 67 else "В очереди"
+        students_to_add.append(StudentListing(student_id=id_, admission_score=score, status=status))
 
     session.add_all(students_to_add)
     await session.commit()
+    await status_update()
 
     return [{"id": student[0], "admission_score": student[1]} for student in student_list]
