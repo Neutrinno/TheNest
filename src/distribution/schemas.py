142cc4135ca04a00c8983181254cea0b48beb58c
bsr <mursik1602@yandex.ru>
2024-11-23 21:40:47 +0500

    gt-->ge

--- src/distribution/schemas.py
+++ src/distribution/schemas.py
@@ -4,4 +4,4 @@ from pydantic import ConfigDict, Field, BaseModel
 class StudentList(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    admission_score: int = Field(gt=0, lt=101)
+    admission_score: int = Field(ge=0, lt=101)
