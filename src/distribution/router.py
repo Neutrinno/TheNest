677ba0661871a99ce264019559902fff8f3e8c2c
bsr <mursik1602@yandex.ru>
2024-11-23 11:08:58 +0500

    Created new tag (wishes)

--- src/distribution/router.py
+++ src/distribution/router.py
@@ -1,5 +1,8 @@
+from importlib.metadata import distribution
+
 from fastapi import APIRouter
 from fastapi.params import Depends
+from pygments.lexer import default
 from sqlalchemy import Result, select, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,6 +11,17 @@ from src.distribution.schemas import StudentList
 from src.models import Application, StudentListing, Status
 
 router = APIRouter()
+
+
+async def choose_room():
+    pass
+
+async def choose_floor():
+    pass
+
+async def choose_dormitory():
+    pass
+
 
 async def status_update(session: AsyncSession = Depends(get_async_session)):
     query = select(StudentListing).order_by(StudentListing.student_id)
@@ -24,19 +38,52 @@ async def status_update(session: AsyncSession = Depends(get_async_session)):
     return {"massage: Статусы успешно обновлены"}
 
 
+async def first_distribution(session: AsyncSession = Depends(get_async_session)):
+    query = select(StudentListing)
+    result: Result = await session.execute(query)
+    student_list = result.scalars().all()
+
+    for student in student_list:
+        information = await session.execute(select(Application).where(Application.student_id == student.student_id).limit(1))
+        if student.status == "Одоборено":
+            if information.preferred_dormitory is not None:
+                if information.floor is not None:
+                    await choose_floor()
+                pass
+
+
 @router.get("/", response_model=list[StudentList])
 async def get_distribution(session: AsyncSession = Depends(get_async_session)):
+
     query = select(Application.id, Application.admission_score).order_by(desc(Application.admission_score))
     result: Result = await session.execute(query)
     student_list = result.all()
 
     students_to_add = []
     for index, (id_, score) in enumerate(student_list):
-        status = "Одобренно" if index < 67 else "В очереди"
-        students_to_add.append(StudentListing(student_id=id_, admission_score=score, status=status))
+        status = "Одобрено" if index < 80 else "В очереди"
+
+        query = select(Application).where(Application.student_id == index).limit(1)
+        result: Result = await session.execute(query)
+        student_list = result.scalars()
+
+        if Application.preferred_dormitory is not None:
+            wishes = True
+        elif Application.preferred_floor is not None:
+            wishes = True
+        elif Application.first_preferred_student is not None:
+            wishes = True
+        else:
+            wishes = False
+
+        students_to_add.append(StudentListing(student_id=id_,
+                                              admission_score=score,
+                                              status=status,
+                                              wishes=wishes))
 
     session.add_all(students_to_add)
     await session.commit()
     await status_update()
+    await first_distribution()
 
     return [{"id": student[0], "admission_score": student[1]} for student in student_list]
