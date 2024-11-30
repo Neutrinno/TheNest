ad862a69e5f09d11116844824618585920bed59a
bsr <mursik1602@yandex.ru>
2024-12-01 00:47:59 +0500

    Replace fastapi_users instance

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,5 +1,9 @@
+from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import CookieTransport
 from fastapi_users.authentication import AuthenticationBackend, JWTStrategy
+
+from src.auth.manager import get_user_manager
+from src.models import User
 
 cookie_transport = CookieTransport(cookie_name = "dorm", cookie_max_age=3600)
 
@@ -13,3 +17,13 @@ auth_backend = AuthenticationBackend(
     transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_active_user = fastapi_users.current_user(active=True)
+current_user = fastapi_users.current_user(optional=True)
+user_authenticator = fastapi_users.authenticator
+get_current_user_token = user_authenticator.current_user_token(active=True, verified=False)
