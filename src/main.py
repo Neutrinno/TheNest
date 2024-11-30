044957338b66edbf0f6b261c59e4dff492d6d420
bsr <mursik1602@yandex.ru>
2024-12-01 00:46:48 +0500

    Created new endpoints

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
+from typing import Union
+
 import uvicorn
+from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_users.authentication import Strategy, Authenticator
+from fastapi.responses import RedirectResponse
+from fastapi_users.schemas import BaseUserCreate
+
 from src.applications.router import router as application_router
 from src.distribution.router import router as distribution_router
 from src.redistribution.router import router as redistribution_router
-from fastapi import FastAPI, Depends
-from fastapi_users import FastAPIUsers
+from fastapi import FastAPI, Depends, Form, Request, Response, status, HTTPException
+from fastapi_users import FastAPIUsers, BaseUserManager, exceptions
 from src.setting import settings
-from src.auth.auth import auth_backend
+from src.auth.auth import auth_backend, current_active_user, get_current_user_token, current_user
 from src.auth.database import User
 from src.auth.manager import get_user_manager
 from src.auth.schemas import UserRead, UserCreate
-from src.pages.router import router as pages_router
+from src.pages.router import router as pages_router, templates
+from fastapi.staticfiles import StaticFiles
 
 
 """Блок, связанный с регистрацией и  аунтификацией"""
@@ -33,7 +42,6 @@ app.include_router(
     prefix=settings.auth.prefix,
     tags=[settings.auth.tags],
 )
-current_user = fastapi_users.current_user()
 
 @app.get("/protected-route")
 def protected_route(user: User = Depends(current_user)):
@@ -64,6 +72,28 @@ app.include_router(
     prefix=settings.pages.prefix,
     tags=[settings.pages.tags]
 )
+
+#app.mount("/src/static", StaticFiles(directory="src/static"), name="static")
+
+
+async def auth_redirect(user: User = Depends(current_user)):
+    if user is None:
+        raise HTTPException(status_code=302, detail="Not authorized", headers={"Location": "/auth"})
+    return user
+
+
+@app.get("/")
+async def authorized(request: Request, user: User = Depends(auth_redirect)):
+    return templates.TemplateResponse(name="authorized.html", request=request)
+
+
+@app.get("/auth")
+async def get_auth(request: Request, user: User = Depends(current_user)):
+    if user is not None:
+        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+    return templates.TemplateResponse(name="auth.html", request=request)
+
+
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", reload=True)
--- src/pages/router.py
+++ src/pages/router.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-
+from src. auth. manager import get_user_manager
 router = APIRouter()
 
 templates = Jinja2Templates(directory='src/templates')
 
 @router.get('/base')
-def get_students_html(request: Request):
+def get_base_html(request: Request):
     return templates.TemplateResponse(name='base.html', context={'request': request})
+
+@router.get('/search')
+def get_search_html(request: Request):
+    return templates.TemplateResponse(name='search.html', context={'request': request})
+
+"""@router.get('/register', register = Depends(get_user_manager))
+def get_register_html(request: Request):
+    return templates.TemplateResponse(name='register.html', context={'request': request})"""
