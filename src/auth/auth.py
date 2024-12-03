c86a6d036f6b139491c56ae69e7b64b0917e5a9a
bsr <mursik1602@yandex.ru>
2024-12-03 18:03:56 +0500

    Изменил вид функций

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -12,16 +12,11 @@ SECRET = "SECRET"
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
-auth_backend = AuthenticationBackend(
-    name="jwt",
-    transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
-)
+auth_backend = AuthenticationBackend(name="jwt",
+                                     transport=cookie_transport,
+                                     get_strategy=get_jwt_strategy)
 
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
+fastapi_users = FastAPIUsers[User, int](get_user_manager,[auth_backend])
 
 current_active_user = fastapi_users.current_user(active=True)
 current_user = fastapi_users.current_user(optional=True)
