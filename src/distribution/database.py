39d9d3fe33d3416bb56886849bad4f7a26bc0eee
bsr <mursik1602@yandex.ru>
2024-12-05 17:18:50 +0500

    \

--- src/distribution/database.py
+++ src/distribution/database.py
@@ -1,8 +1,6 @@
 from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-
 from src.config import DB_USER, DB_PASS, DB_PORT, DB_NAME, DB_HOST
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
