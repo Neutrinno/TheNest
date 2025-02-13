6ef0b392ba0affb6415619edd4b7a023a34c7973
bsr <mursik1602@yandex.ru>
2025-02-14 00:18:54 +0500

    Added CORS middleware

--- src/main.py
+++ src/main.py
@@ -7,8 +7,19 @@ from fastapi import FastAPI
 from src.setting import settings
 from src.pages.router import router as pages_router
 from src.auth.router import router as auth_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title = 'TheNest')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     application_router,
